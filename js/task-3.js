124304066f1b2505a0bfc8d06f551d59c3d80ef5
Julia K <yy.caramella@gmail.com>
2026-04-06 13:55:31 +0300

    first commit

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,25 @@
-function getElementWidth(content, padding, border) {
-  const contentValue = parseFloat(content);
-  const paddingValue = parseFloat(padding);
-  const borderValue = parseFloat(border);
+const profile = {
+  username: "Jacob",
+  playTime: 300,
 
-  return contentValue + paddingValue * 2 + borderValue * 2;
-}
+  changeUsername(newName) {
+    this.username = newName;
+  },
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+// Перевірка (залиш для ментора)
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
